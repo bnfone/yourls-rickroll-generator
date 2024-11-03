fa48f02a04dfa6699a79c291470cdf0c18423d4c
bnfone <89687390+bnfone@users.noreply.github.com>
2024-11-03 02:31:48 +0100

    Update plugin.php
    additional menu

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Rickroll Generator
 Plugin URI: https://github.com/bnfone/yourls-rickroll-generator
 Description: Erstelle Rickroll-Links mit benutzerdefinierten Vorschaumetadaten
-Version: 1.0
+Version: 1.1
 Author: Benedikt Fischer
 Author URI: https://bnfone.com/
 */
@@ -27,6 +27,7 @@ yourls_add_action('plugins_loaded', 'rickroll_init');
 function rickroll_init()
 {
     yourls_register_plugin_page('rickroll', 'Rickroll Generator', 'rickroll_display_page');
+    yourls_register_plugin_page('rickroll_links', 'Rickroll Links', 'rickroll_display_links_page');
 }
 
 // Anzeige der Plugin-Admin-Seite
@@ -193,5 +194,79 @@ function rickroll_show_preview($args)
     </html>
     <?php
     exit(); // Normale Weiterleitung stoppen
+}
+
+// Funktion zum Abrufen der Rickroll-Links
+function get_rickroll_links()
+{
+    $table_url = YOURLS_DB_TABLE_URL;
+
+    $sql = "SELECT * FROM `$table_url`";
+    $links = yourls_get_db()->fetchObjects($sql);
+
+    $rickroll_links = array();
+
+    foreach ($links as $link) {
+        $meta = json_decode($link->title);
+        if ($meta && isset($meta->title) && isset($meta->description) && isset($meta->image)) {
+            // Dies ist ein Rickroll-Link
+            $rickroll_links[] = $link;
+        }
+    }
+
+    return $rickroll_links;
+}
+
+// Anzeige der Rickroll-Links-Seite
+function rickroll_display_links_page()
+{
+    // Wenn ein Link gelöscht werden soll
+    if (isset($_GET['action']) && $_GET['action'] == 'delete' && isset($_GET['keyword'])) {
+        yourls_verify_nonce('rickroll_delete_link');
+        $keyword = yourls_sanitize_keyword($_GET['keyword']);
+        $result = yourls_delete_link_by_keyword($keyword);
+        if ($result) {
+            echo '<p>Link mit dem Keyword ' . htmlspecialchars($keyword) . ' wurde gelöscht.</p>';
+        } else {
+            echo '<p>Fehler beim Löschen des Links mit dem Keyword ' . htmlspecialchars($keyword) . '.</p>';
+        }
+    }
+
+    // Rickroll-Links abrufen
+    $links = get_rickroll_links();
+
+    // Zähler
+    $total_links = count($links);
+    $total_clicks = 0;
+
+    echo '<h2>Rickroll-Links</h2>';
+    echo '<p>Anzahl der Rickroll-Links: ' . $total_links . '</p>';
+
+    if ($total_links > 0) {
+        echo '<table border="1" cellpadding="5" cellspacing="0">';
+        echo '<tr><th>Keyword</th><th>URL</th><th>Klicks</th><th>Aktion</th></tr>';
+
+        foreach ($links as $link) {
+            $keyword = $link->keyword;
+            $url = $link->url;
+            $clicks = $link->clicks;
+            $total_clicks += $clicks;
+            $delete_link = yourls_nonce_url('rickroll_delete_link', yourls_admin_url('plugins.php?page=rickroll_links&action=delete&keyword=' . $keyword));
+
+            echo '<tr>';
+            echo '<td>' . htmlspecialchars($keyword) . '</td>';
+            echo '<td><a href="' . htmlspecialchars($url) . '">' . htmlspecialchars($url) . '</a></td>';
+            echo '<td>' . $clicks . '</td>';
+            echo '<td><a href="' . $delete_link . '" onclick="return confirm(\'Möchten Sie diesen Link wirklich löschen?\')">Löschen</a></td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+
+        echo '<p>Gesamte Rickroll-Klicks: ' . $total_clicks . '</p>';
+
+    } else {
+        echo '<p>Es wurden noch keine Rickroll-Links erstellt.</p>';
+    }
 }
 ?>
